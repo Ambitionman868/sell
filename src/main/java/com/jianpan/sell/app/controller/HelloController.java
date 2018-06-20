06efef71a4ad67197e763ccdbf0dfb9990fa3d06
Jianpan <497231226@qq.com>
2018-06-20 22:09:52 +0800

    Initial commit

---
+++ src/main/java/com/jianpan/sell/app/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.jianpan.sell.app.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    private String hello() {
+
+        return "hello";
+    }
+}
